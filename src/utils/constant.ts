c8ed1bba8a824c2faa88f31c8df95cee8c20f243
Vaibhav Thakur <vibhu1108@gmail.com>
2026-07-01 12:49:39 +0530

    chore: remove un-used files and folder

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,16 +1,18 @@
+import type { DropdownOption, SortType } from "../types";
+
 export const NETWORK_DELAY = 500;
 
 export const PRODUCTS_PER_PAGE = 8;
 
-export const ratingOptions = [
+export const ratingOptions: DropdownOption<number>[] = [
   { label: "All Ratings", value: 0 },
-  { label: "4★ & Above", value: 4 },
-  { label: "3★ & Above", value: 3 },
-  { label: "2★ & Above", value: 2 },
-  { label: "1★ & Above", value: 1 },
+  { label: "4★", value: 4 },
+  { label: "3★", value: 3 },
+  { label: "2★", value: 2 },
+  { label: "1★", value: 1 },
 ];
 
-export const sortOptions = [
+export const sortOptions: DropdownOption<SortType>[] = [
   { label: "Price: Low to High", value: "asc" },
   { label: "Price: High to Low", value: "desc" },
 ];
